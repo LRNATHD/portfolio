28e9fb27e9410bbc48e8a80ba04d1e87ef90dfbb
LRNATHD <LRNATHD@users.noreply.github.com>
2026-09-14 08:47:24 -0400

    Change final project path to PCB-CNC-Multitool

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        highschoolfinal: resolve(__dirname, 'highschoolfinal/index.html'),
+        'PCB-CNC-Multitool': resolve(__dirname, 'PCB-CNC-Multitool/index.html'),
       },
     },
   },
