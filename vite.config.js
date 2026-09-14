9333332f00cacd515232b308dfa9ea25098e2a2c
LRNATHD <LRNATHD@users.noreply.github.com>
2026-09-14 08:44:35 -0400

    Add highschoolfinal placeholder page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        highschoolfinal: resolve(__dirname, 'highschoolfinal/index.html'),
+      },
+    },
   },
   assetsInclude: ['**/*.md'],
 });
